95a97f29b665ac011f63f78a944a1ec573e9e86d
mizunakakiyama <mizunakakiyama@gmail.com>
2026-03-30 16:30:52 +0200

    Add files via upload

--- js/okez.js
+++ js/okez.js
@@ -26,7 +26,7 @@ async function code(text) {
   console.log("checkpoint 1");
 
   msg_a = msg.split("").map((c) => c.charCodeAt(0));
-  console.log(`default msg: ${msg_a}`);
+
   const rep = await fetch("./0000.txt");
   const letext = await rep.text();
   const lines = letext.split("\n");
@@ -34,7 +34,7 @@ async function code(text) {
   for (var i = 0; i < msg_a.length; i++) {
     msg_f += msg_a[i];
   }
-
+  console.log(`default msg: ${msg_a}`);
   lines.forEach((line) => {
     var cur_line = line.split(":");
     if (cur_line[1] === msg_f) {
