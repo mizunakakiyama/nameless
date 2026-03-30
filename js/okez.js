6100b99e3cfa1cd80545e8442c3a4ac9680ce9e4
mizunakakiyama <mizunakakiyama@gmail.com>
2026-03-30 16:16:28 +0200

    Add files via upload

--- js/okez.js
+++ js/okez.js
@@ -13,7 +13,7 @@ document.getElementById("textbox").addEventListener("input", function (e) {
 document.addEventListener("keypress", (e) => {
   if (e.code === "Enter" && document.getElementById("textbox").value != null) {
     console.log("checkpoint 0");
-    code(document.getElementById("textbox").value);
+    code(realValue);
   }
 });
 
